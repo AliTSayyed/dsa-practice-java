fde61b3c708e43388f8ec5792f695facea3d0d26
Ali-Taabesh Sayyed <ats@AliTaabeshsMini.attlocal.net>
2025-07-10 22:06:01 -0500

    Completed concordance bst assignment

---
+++ src/UtdDsa/concordance/Concordance.java
@@ -0,0 +1,106 @@
+package UtdDsa.concordance;
+// take in one line at a time (lines are a linked list)
+
+// regexp the line to get rid of digits and anything not a letter, ' "" - 
+// create bst tree
+// print alphabetical order of all words, their frequency, and what lines they appear on
+// linked lists should be 1-2-3... not 1-1-1-.. if they appear on the same line 
+
+// Node that stores the line number
+class listNode {
+    int num;
+    listNode next;
+
+    public listNode(int num) {
+        this.num = num;
+    }
+
+}
+
+class treeNode {
+    String word;
+    treeNode left;
+    treeNode right;
+    listNode lines;
+    int frequency;
+
+    // when we come across a new word we create a new treeNode
+    // tree node will contian the word, have an initial frequency of 1, and store
+    // the line number it was found on.
+    public treeNode(String word, int lineNumber) {
+        this.word = word;
+        frequency = 1;
+        lines = new listNode(lineNumber);
+    }
+
+    public void printLineNumbers() {
+        listNode current = lines;
+        while (current != null) {
+            System.out.print(current.num + " ");
+            current = current.next;
+        }
+    }
+}
+
+// create the binary search tree with recursion and comparing strings
+public class Concordance {
+    // Store the root of the tree
+    treeNode root;
+
+    // create a method to insert a word and its line number
+    // compare the word from the root node, if the word les less go left, if it is
+    // greater go right and if it is the same word, we increment the frequency, and
+    // make lines.next = new listNode(lineNumber) IF the line number is not the same
+    // as the current line (i.e 1-2-3 not 1-1-2)
+    // use recursion to travel down the tree and return the root
+    public void insert(String word, int lineNumber) {
+        root = insertRec(root, word, lineNumber);
+    }
+
+    private treeNode insertRec(treeNode root, String word, int lineNumber) {
+        if (root == null) {
+            root = new treeNode(word, lineNumber);
+        } else {
+            int comparison = word.compareTo(root.word);
+            if (comparison < 0) {
+                root.left = insertRec(root.left, word, lineNumber);
+            } else if (comparison > 0) {
+                root.right = insertRec(root.right, word, lineNumber);
+            } else if (comparison == 0) {
+                root.frequency++;
+                // traverse the linked list, if the line number is not there, then add the new
+                // listNode lineNumber
+                // use a current node for traversing
+                listNode current = root.lines;
+                while (true) {
+                    if (current.num == lineNumber) {
+                        break;
+                    } else if (current.next == null) {
+                        current.next = new listNode(lineNumber);
+                        break;
+                    } else {
+                        current = current.next;
+                    }
+                }
+            }
+        }
+        return root;
+    }
+
+    // create a method to print the nodes in order
+    // print the word, its frequency, and the line numbers it showed on
+    public void printTree() {
+        inOrderPrintRec(root);
+    }
+
+    private void inOrderPrintRec(treeNode root) {
+        if (root != null) {
+            inOrderPrintRec(root.left);
+            System.out.print("Word: " + root.word + ", " + "Frequency: " + root.frequency + ", Found on Lines: ");
+            root.printLineNumbers();
+            System.out.println();
+            inOrderPrintRec(root.right);
+        }
+    }
+
+}
--- src/UtdDsa/concordance/Main.java
+++ src/UtdDsa/concordance/Main.java
@@ -1,17 +1,43 @@
 package UtdDsa.concordance;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // create scanner input for file
+        System.out.println("Current directory: " + System.getProperty("user.dir"));
+        try {
+            // take in the txt file
+            File file = new File("src/UtdDsa/concordance/test2.txt");
+            Scanner scanner = new Scanner(file);
+            Concordance binaryTree = new Concordance();
+            int lineNumber = 1;
 
-        // read one word at a time
-        // must clean the word of punctuation and make them all lower case
+            // take in one line at a time keeping track of the line number
+            // read one word at a time
+            // must clean the word of punctuation and make them all lower case
+            // for each word insert it into the tree using the insert method with the line
+            // number
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
 
-        // for each word insert it into the tree using the insert method (takes care of
-        // freq)
-        // insert the word and line number
+                String[] words = line.split(" ");
 
-        // print out the tree (words in order with their frequency and line numbers)
+                for (String word : words) {
+                    word = word.replaceAll("[^a-zA-Z'-]", "").toLowerCase();
+                    if (!word.isEmpty()) {
+                        binaryTree.insert(word, lineNumber);
+                    }
+                }
+                lineNumber++;
+            }
+            // print out the tree
+            binaryTree.printTree();
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("There was an error:" + e);
+        }
+
     }
 }
